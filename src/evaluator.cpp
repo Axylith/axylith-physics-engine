a738f8ea411a5fd1415335fd166258545b8e7603
Mathemusician1729 <tranducmatthew@gmail.com>
2026-05-14 00:35:38 -0600

    added dependabot

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -4,7 +4,7 @@
 EvalResult evaluate(const std::string& expression) {
     try {
         if (expression == "1+1" || expression == "1 + 1") {
-            return {2.0, "", 0, ""};
+            return {2, "", 0, ""};
         }
     } catch (const std::exception& e) {
         return {0.0, "", -1, "Simple parser failed"};
