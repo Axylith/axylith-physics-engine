5349025f52d47292e5be389d524bc1f7d94a3d65
Matthew <tranducmatthew@gmail.com>
2026-06-07 16:35:16 -0600

    units setup

---
+++ include/units.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <array>
+
+namespace axylith {
+    // define unit dimensions (using the 7 SI units, storing the value of the exponent of each)
+    // e.g. m/s^2 would be [0, 1, -2, 0, 0, 0, 0]
+    // order is [mass, distance, time, current, temperature, mols, candela]
+    struct UnitComponents { 
+        std::array<int8_t, 7> units{0};
+    };
+
+    struct Unit { // define entire unit (its quantity + unit) --> e.g. '5' and 'kg'
+        double value; // value of the unit
+        UnitComponents u; // components of the unit
+    };
+}
